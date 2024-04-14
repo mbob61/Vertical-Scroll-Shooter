ff66399f0156bec2a65a402a017cbda5a6bac0a3
Michael Souster <michael.souster@live.co.uk>
2024-04-14 11:18:33 +0100

    cleaned up scene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private GameObject floorTileParent;
+    [SerializeField] private GameObject backgroundTileParent;
+    [SerializeField] private GameObject wallPiecesParent;
+
     [SerializeField] private GameObject leftWallPiece;
     [SerializeField] private GameObject rightWallPiece;
     [SerializeField] private GameObject backroundWallPiece;
@@ -26,32 +30,69 @@ public class GameManager : MonoBehaviour
 
         for (int i = 0; i <= topRightWorld.y * 2 + 1; i++)
         {
-            Instantiate(leftWallPiece, new Vector3(bottomLeftWorld.x / 2, bottomLeftWorld.y + i), Quaternion.identity);
-            Instantiate(rightWallPiece, new Vector3(topRightWorld.x / 2, bottomLeftWorld.y + i), Quaternion.identity);
+            CreateWallTiles(i);
+            createFloorTiles(i);
+            createBackgroundTiles(i);
 
-            Instantiate(floorPiece, new Vector3(bottomLeftWorld.x / 2 + 1, bottomLeftWorld.y + i), Quaternion.identity);
-            Instantiate(floorPiece, new Vector3(bottomLeftWorld.x / 2 + 2, bottomLeftWorld.y + i), Quaternion.identity);
-            Instantiate(floorPiece, new Vector3(bottomLeftWorld.x / 2 + 3, bottomLeftWorld.y + i), Quaternion.identity);
-            Instantiate(floorPiece, new Vector3(bottomLeftWorld.x / 2 + 4, bottomLeftWorld.y + i), Quaternion.identity);
-
-            Instantiate(floorPiece, new Vector3(bottomLeftWorld.x / 2 + 5, bottomLeftWorld.y + i), Quaternion.identity);
-            Instantiate(floorPiece, new Vector3(bottomLeftWorld.x / 2 + 6, bottomLeftWorld.y + i), Quaternion.identity);
-            Instantiate(floorPiece, new Vector3(bottomLeftWorld.x / 2 + 7, bottomLeftWorld.y + i), Quaternion.identity);
-            Instantiate(floorPiece, new Vector3(bottomLeftWorld.x / 2 + 8, bottomLeftWorld.y + i), Quaternion.identity);
-
-            Instantiate(backroundWallPiece, new Vector3(bottomLeftWorld.x / 2 - 1, bottomLeftWorld.y + i), Quaternion.identity);
-            Instantiate(backroundWallPiece, new Vector3(bottomLeftWorld.x / 2 - 2, bottomLeftWorld.y + i), Quaternion.identity);
-            Instantiate(backroundWallPiece, new Vector3(bottomLeftWorld.x / 2 - 3, bottomLeftWorld.y + i), Quaternion.identity);
-            Instantiate(backroundWallPiece, new Vector3(bottomLeftWorld.x / 2 - 4, bottomLeftWorld.y + i), Quaternion.identity);
-            Instantiate(backroundWallPiece, new Vector3(bottomLeftWorld.x / 2 - 5, bottomLeftWorld.y + i), Quaternion.identity);
-
-            Instantiate(backroundWallPiece, new Vector3(topRightWorld.x / 2 + 1, bottomLeftWorld.y + i), Quaternion.identity);
-            Instantiate(backroundWallPiece, new Vector3(topRightWorld.x / 2 + 2, bottomLeftWorld.y + i), Quaternion.identity);
-            Instantiate(backroundWallPiece, new Vector3(topRightWorld.x / 2 + 3, bottomLeftWorld.y + i), Quaternion.identity);
-            Instantiate(backroundWallPiece, new Vector3(topRightWorld.x / 2 + 4, bottomLeftWorld.y + i), Quaternion.identity);
-            Instantiate(backroundWallPiece, new Vector3(topRightWorld.x / 2 + 5, bottomLeftWorld.y + i), Quaternion.identity);
             lastPosition.y = bottomLeftWorld.y + i + 1;
         }
+    }
+
+    private void CreateWallTiles(int i)
+    {
+        GameObject wallTile1 = Instantiate(leftWallPiece, new Vector3(bottomLeftWorld.x / 2, bottomLeftWorld.y + i), Quaternion.identity);
+        GameObject wallTile2 = Instantiate(rightWallPiece, new Vector3(topRightWorld.x / 2, bottomLeftWorld.y + i), Quaternion.identity);
+
+        wallTile1.transform.parent = wallPiecesParent.transform;
+        wallTile2.transform.parent = wallPiecesParent.transform;
+
+    }
+
+    private void createFloorTiles(int i)
+    {
+        GameObject floor1 = Instantiate(floorPiece, new Vector3(bottomLeftWorld.x / 2 + 1, bottomLeftWorld.y + i), Quaternion.identity);
+        GameObject floor2 = Instantiate(floorPiece, new Vector3(bottomLeftWorld.x / 2 + 2, bottomLeftWorld.y + i), Quaternion.identity);
+        GameObject floor3 = Instantiate(floorPiece, new Vector3(bottomLeftWorld.x / 2 + 3, bottomLeftWorld.y + i), Quaternion.identity);
+        GameObject floor4 = Instantiate(floorPiece, new Vector3(bottomLeftWorld.x / 2 + 4, bottomLeftWorld.y + i), Quaternion.identity);
+        GameObject floor5 = Instantiate(floorPiece, new Vector3(bottomLeftWorld.x / 2 + 5, bottomLeftWorld.y + i), Quaternion.identity);
+        GameObject floor6 = Instantiate(floorPiece, new Vector3(bottomLeftWorld.x / 2 + 6, bottomLeftWorld.y + i), Quaternion.identity);
+        GameObject floor7 = Instantiate(floorPiece, new Vector3(bottomLeftWorld.x / 2 + 7, bottomLeftWorld.y + i), Quaternion.identity);
+        GameObject floor8 = Instantiate(floorPiece, new Vector3(bottomLeftWorld.x / 2 + 8, bottomLeftWorld.y + i), Quaternion.identity);
+
+        floor1.transform.parent = floorTileParent.transform;
+        floor2.transform.parent = floorTileParent.transform;
+        floor3.transform.parent = floorTileParent.transform;
+        floor4.transform.parent = floorTileParent.transform;
+        floor5.transform.parent = floorTileParent.transform;
+        floor6.transform.parent = floorTileParent.transform;
+        floor7.transform.parent = floorTileParent.transform;
+        floor8.transform.parent = floorTileParent.transform;
+    }
+
+    private void createBackgroundTiles(int i)
+    {
+        GameObject backgroundPeice1 = Instantiate(backroundWallPiece, new Vector3(bottomLeftWorld.x / 2 - 1, bottomLeftWorld.y + i), Quaternion.identity);
+        GameObject backgroundPeice2 = Instantiate(backroundWallPiece, new Vector3(bottomLeftWorld.x / 2 - 2, bottomLeftWorld.y + i), Quaternion.identity);
+        GameObject backgroundPeice3 = Instantiate(backroundWallPiece, new Vector3(bottomLeftWorld.x / 2 - 3, bottomLeftWorld.y + i), Quaternion.identity);
+        GameObject backgroundPeice4 = Instantiate(backroundWallPiece, new Vector3(bottomLeftWorld.x / 2 - 4, bottomLeftWorld.y + i), Quaternion.identity);
+        GameObject backgroundPeice5 = Instantiate(backroundWallPiece, new Vector3(bottomLeftWorld.x / 2 - 5, bottomLeftWorld.y + i), Quaternion.identity);
+        GameObject backgroundPeice6 = Instantiate(backroundWallPiece, new Vector3(topRightWorld.x / 2 + 1, bottomLeftWorld.y + i), Quaternion.identity);
+        GameObject backgroundPeice7 = Instantiate(backroundWallPiece, new Vector3(topRightWorld.x / 2 + 2, bottomLeftWorld.y + i), Quaternion.identity);
+        GameObject backgroundPeice8 = Instantiate(backroundWallPiece, new Vector3(topRightWorld.x / 2 + 3, bottomLeftWorld.y + i), Quaternion.identity);
+        GameObject backgroundPeice9 = Instantiate(backroundWallPiece, new Vector3(topRightWorld.x / 2 + 4, bottomLeftWorld.y + i), Quaternion.identity);
+        GameObject backgroundPeice10 = Instantiate(backroundWallPiece, new Vector3(topRightWorld.x / 2 + 5, bottomLeftWorld.y + i), Quaternion.identity);
+
+        backgroundPeice1.transform.parent = backgroundTileParent.transform;
+        backgroundPeice2.transform.parent = backgroundTileParent.transform;
+        backgroundPeice3.transform.parent = backgroundTileParent.transform;
+        backgroundPeice4.transform.parent = backgroundTileParent.transform;
+        backgroundPeice5.transform.parent = backgroundTileParent.transform;
+        backgroundPeice6.transform.parent = backgroundTileParent.transform;
+        backgroundPeice7.transform.parent = backgroundTileParent.transform;
+        backgroundPeice8.transform.parent = backgroundTileParent.transform;
+        backgroundPeice9.transform.parent = backgroundTileParent.transform;
+        backgroundPeice10.transform.parent = backgroundTileParent.transform;
+
     }
 
     // Update is called once per frame
