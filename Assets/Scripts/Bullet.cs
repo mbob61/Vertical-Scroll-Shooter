2a1fff75448fd629c70989df7b6f787f267305d4
MTiramani <53878051+MTiramani@users.noreply.github.com>
2024-04-14 09:01:58 +0100

    Added initial files, basic 2d scrolling

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    [SerializeField] private float bulletSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position +=new Vector3(0, bulletSpeed * Time.deltaTime);
+    }
+}
