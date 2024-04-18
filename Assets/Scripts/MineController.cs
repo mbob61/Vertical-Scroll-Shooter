bb7b9c6257b7832646bb439de3e1b54263554963
MTiramani <53878051+MTiramani@users.noreply.github.com>
2024-04-18 20:55:24 +0100

    Added Mines and Health pickups as a choice

---
+++ Assets/Scripts/MineController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineController : MonoBehaviour
+{
+    [SerializeField] private int damage;
+    [SerializeField] private GameObject particles;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            PlayerController player = collision.GetComponent<PlayerController>();
+            player.DecrementHealth(damage);
+            Instantiate(particles, this.transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+        }
+    }
+}
